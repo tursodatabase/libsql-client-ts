d45b8bff20fd329258e12d68eff845e7dca2812d
Jan Špaček <patek.mail@gmail.com>
2023-03-27 10:42:07 +0200

    Use aiohttp.web for websockets

--- testing/hrana-test-server/test_server.py
+++ testing/hrana-test-server/test_server.py
@@ -2,39 +2,53 @@ import asyncio
 import base64
 import collections
 import json
+import logging
 import os
 import sqlite3
 import sys
 import tempfile
 
-import websockets
+import aiohttp.web
 
-class ResponseError(RuntimeError):
-    pass
+logger = logging.getLogger("test_server")
 
-async def main():
-    server = await websockets.serve(handle_socket, "localhost", 2023, subprotocols=["hrana1"])
-    for sock in server.sockets:
-        print(f"Listening on {sock.getsockname()!r}")
-    await server.wait_closed()
+def main():
+    logging.basicConfig(level=logging.INFO)
+    app = aiohttp.web.Application()
+    app.add_routes([
+        aiohttp.web.get("/", handle_get_index),
+        #aiohttp.web.post("/v1/execute", handle_post_execute),
+        #aiohttp.web.post("/v1/batch", handle_post_batch),
+    ])
+    aiohttp.web.run_app(app, host="localhost", port=8080)
 
-async def handle_socket(websocket):
+async def handle_get_index(req):
+    ws = aiohttp.web.WebSocketResponse(protocols=("hrana1",))
+    if ws.can_prepare(req):
+        await ws.prepare(req)
+        await handle_websocket(ws)
+        return ws
+
+    return aiohttp.web.Response(text="This is a Hrana test server")
+
+async def handle_websocket(ws):
     async def recv_msg():
-        try:
-            msg_str = await websocket.recv()
-        except websockets.exceptions.ConnectionClosed:
+        ws_msg = await ws.receive()
+        if ws_msg.type == aiohttp.WSMsgType.TEXT:
+            msg = json.loads(ws_msg.data)
+            return msg
+        elif ws_msg.type in (aiohttp.WSMsgType.CLOSE, aiohttp.WSMsgType.CLOSED):
             return None
-        assert isinstance(msg_str, str)
-        msg = json.loads(msg_str)
-        return msg
+        else:
+            raise RuntimeError(f"Unknown websocket message: {msg!r}")
 
     async def send_msg(msg):
         msg_str = json.dumps(msg)
-        await websocket.send(msg_str)
+        await ws.send_str(msg_str)
 
     db_fd, db_file = tempfile.mkstemp(suffix=".db", prefix="hrana_client_test_")
     os.close(db_fd)
-    print(f"Accepted connection from {websocket.remote_address}, using db {db_file!r}")
+    logger.info(f"Accepted WebSocket using db {db_file!r}")
 
     Stream = collections.namedtuple("Stream", ["conn"])
     streams = {}
@@ -60,119 +74,6 @@ async def handle_socket(websocket):
             return {"type": "batch", "result": result}
         else:
             raise RuntimeError(f"Unknown req: {req!r}")
-
-    def execute_stmt(conn, stmt):
-        args = stmt.get("args", [])
-        named_args = stmt.get("named_args", [])
-        if len(named_args) == 0:
-            sql_args = [value_to_sqlite(arg) for arg in args]
-        elif len(args) == 0:
-            sql_args = {}
-            for arg in named_args:
-                value = value_to_sqlite(arg["value"])
-                if arg["name"][0] in (":", "@", "$"):
-                    key = arg["name"][1:]
-                else:
-                    key = arg["name"]
-                sql_args[key] = value
-        else:
-            raise RuntimeError(f"Using both positional and named arguments is not supported")
-
-        try:
-            cursor = conn.execute(stmt["sql"], sql_args)
-        except sqlite3.OperationalError as e:
-            raise ResponseError(str(e))
-        cols = [{"name": name} for name, *_ in cursor.description or []]
-
-        rows = []
-        for row in cursor:
-            if stmt["want_rows"]:
-                rows.append([value_from_sqlite(val) for val in row])
-
-        if cursor.rowcount >= 0:
-            affected_row_count = cursor.rowcount
-        else:
-            affected_row_count = 0
-
-        if cursor.lastrowid is not None:
-            last_insert_rowid = str(cursor.lastrowid)
-        else:
-            last_insert_rowid = None
-
-        return {
-            "cols": cols,
-            "rows": rows,
-            "affected_row_count": affected_row_count,
-            "last_insert_rowid": last_insert_rowid,
-        }
-
-    async def execute_batch(conn, batch):
-        step_results = []
-        step_errors = []
-        for step in batch["steps"]:
-            condition = step.get("condition")
-            if condition is not None:
-                enabled = eval_cond(step_results, step_errors, condition)
-            else:
-                enabled = True
-
-            step_result = None
-            step_error = None
-            if enabled:
-                try:
-                    step_result = await asyncio.to_thread(lambda: execute_stmt(conn, step["stmt"]))
-                except ResponseError as e:
-                    step_error = {"message": str(e)}
-
-            step_results.append(step_result)
-            step_errors.append(step_error)
-
-        return {
-            "step_results": step_results,
-            "step_errors": step_errors,
-        }
-
-    def eval_cond(step_results, step_errors, cond):
-        if cond["type"] == "ok":
-            return step_results[cond["step"]] is not None
-        elif cond["type"] == "error":
-            return step_errors[cond["step"]] is not None
-        elif cond["type"] == "not":
-            return not eval_cond(step_results, step_errors, cond["cond"])
-        elif cond["type"] == "and":
-            return all(eval_cond(step_results, step_errors, c) for c in cond["conds"])
-        elif cond["type"] == "or":
-            return any(eval_cond(step_results, step_errors, c) for c in cond["conds"])
-        else:
-            raise RuntimeError(f"Unknown cond: {cond!r}")
-
-    def value_to_sqlite(value):
-        if value["type"] == "null":
-            return None
-        elif value["type"] == "integer":
-            return int(value["value"])
-        elif value["type"] == "float":
-            return float(value["value"])
-        elif value["type"] == "text":
-            return str(value["value"])
-        elif value["type"] == "blob":
-            return base64.b64decode(value["base64"])
-        else:
-            raise RuntimeError(f"Unknown value: {value!r}")
-
-    def value_from_sqlite(value):
-        if value is None:
-            return {"type": "null"}
-        elif isinstance(value, int):
-            return {"type": "integer", "value": str(value)}
-        elif isinstance(value, float):
-            return {"type": "float", "value": value}
-        elif isinstance(value, str):
-            return {"type": "text", "value": value}
-        elif isinstance(value, bytes):
-            return {"type": "blob", "base64": base64.b64encode(value).decode()}
-        else:
-            raise RuntimeError(f"Unknown SQLite value: {value!r}")
 
     async def handle_msg(msg):
         if msg["type"] == "request":
@@ -203,14 +104,126 @@ async def handle_socket(websocket):
             if msg is None:
                 break
             await handle_msg(msg)
-    except websockets.exceptions.ConnectionClosedError:
-        pass
     finally:
         for stream in streams.values():
             stream.conn.close()
         os.unlink(db_file)
 
-try:
-    asyncio.run(main())
-except KeyboardInterrupt:
-    print()
+def execute_stmt(conn, stmt):
+    args = stmt.get("args", [])
+    named_args = stmt.get("named_args", [])
+    if len(named_args) == 0:
+        sql_args = [value_to_sqlite(arg) for arg in args]
+    elif len(args) == 0:
+        sql_args = {}
+        for arg in named_args:
+            value = value_to_sqlite(arg["value"])
+            if arg["name"][0] in (":", "@", "$"):
+                key = arg["name"][1:]
+            else:
+                key = arg["name"]
+            sql_args[key] = value
+    else:
+        raise RuntimeError(f"Using both positional and named arguments is not supported")
+
+    try:
+        cursor = conn.execute(stmt["sql"], sql_args)
+    except sqlite3.OperationalError as e:
+        raise ResponseError(str(e))
+    cols = [{"name": name} for name, *_ in cursor.description or []]
+
+    rows = []
+    for row in cursor:
+        if stmt["want_rows"]:
+            rows.append([value_from_sqlite(val) for val in row])
+
+    if cursor.rowcount >= 0:
+        affected_row_count = cursor.rowcount
+    else:
+        affected_row_count = 0
+
+    if cursor.lastrowid is not None:
+        last_insert_rowid = str(cursor.lastrowid)
+    else:
+        last_insert_rowid = None
+
+    return {
+        "cols": cols,
+        "rows": rows,
+        "affected_row_count": affected_row_count,
+        "last_insert_rowid": last_insert_rowid,
+    }
+
+async def execute_batch(conn, batch):
+    step_results = []
+    step_errors = []
+    for step in batch["steps"]:
+        condition = step.get("condition")
+        if condition is not None:
+            enabled = eval_cond(step_results, step_errors, condition)
+        else:
+            enabled = True
+
+        step_result = None
+        step_error = None
+        if enabled:
+            try:
+                step_result = await asyncio.to_thread(lambda: execute_stmt(conn, step["stmt"]))
+            except ResponseError as e:
+                step_error = {"message": str(e)}
+
+        step_results.append(step_result)
+        step_errors.append(step_error)
+
+    return {
+        "step_results": step_results,
+        "step_errors": step_errors,
+    }
+
+def eval_cond(step_results, step_errors, cond):
+    if cond["type"] == "ok":
+        return step_results[cond["step"]] is not None
+    elif cond["type"] == "error":
+        return step_errors[cond["step"]] is not None
+    elif cond["type"] == "not":
+        return not eval_cond(step_results, step_errors, cond["cond"])
+    elif cond["type"] == "and":
+        return all(eval_cond(step_results, step_errors, c) for c in cond["conds"])
+    elif cond["type"] == "or":
+        return any(eval_cond(step_results, step_errors, c) for c in cond["conds"])
+    else:
+        raise RuntimeError(f"Unknown cond: {cond!r}")
+
+def value_to_sqlite(value):
+    if value["type"] == "null":
+        return None
+    elif value["type"] == "integer":
+        return int(value["value"])
+    elif value["type"] == "float":
+        return float(value["value"])
+    elif value["type"] == "text":
+        return str(value["value"])
+    elif value["type"] == "blob":
+        return base64.b64decode(value["base64"])
+    else:
+        raise RuntimeError(f"Unknown value: {value!r}")
+
+def value_from_sqlite(value):
+    if value is None:
+        return {"type": "null"}
+    elif isinstance(value, int):
+        return {"type": "integer", "value": str(value)}
+    elif isinstance(value, float):
+        return {"type": "float", "value": value}
+    elif isinstance(value, str):
+        return {"type": "text", "value": value}
+    elif isinstance(value, bytes):
+        return {"type": "blob", "base64": base64.b64encode(value).decode()}
+    else:
+        raise RuntimeError(f"Unknown SQLite value: {value!r}")
+
+class ResponseError(RuntimeError):
+    pass
+
+if __name__ == "__main__":
+    main()
