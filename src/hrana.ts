88f1fb177d0dde64e77a4994cb073fd4a48465fc
Jan Špaček <patek.mail@gmail.com>
2023-05-05 09:55:42 +0200

    Store SQL texts on the server with Hrana 2

--- src/hrana.ts
+++ src/hrana.ts
@@ -5,6 +5,7 @@ import { LibsqlError } from "./api.js";
 import type { ExpandedConfig } from "./config.js";
 import { expandConfig } from "./config.js";
 import { supportedUrlLink } from "./help.js";
+import { Lru } from "./lru.js";
 import { encodeBaseUrl } from "./uri.js";
 
 export * from "./api.js";
@@ -49,25 +50,37 @@ export function _createClient(config: ExpandedConfig): HranaClient {
     return new HranaClient(client, url, config.authToken);
 }
 
+const sqlCacheCapacity = 100;
+
 export class HranaClient implements Client {
-    #client: hrana.Client;
     #url: URL;
     #authToken: string | undefined;
     closed: boolean;
 
+    #client: hrana.Client;
+    #sqlCache: Lru<string, hrana.Sql>;
+
     /** @private */
     constructor(client: hrana.Client, url: URL, authToken: string | undefined) {
-        this.#client = client;
         this.#url = url;
         this.#authToken = authToken;
         this.closed = false;
+
+        this.#client = client;
+        this.#sqlCache = new Lru();
     }
 
     async execute(stmt: InStatement): Promise<ResultSet> {
+        const useSqlCache = await this._useSqlCache();
         const stream = this.#openStream();
         try {
             const hranaStmt = stmtToHrana(stmt);
+            if (useSqlCache) {
+                this._applySqlCache(hranaStmt);
+            }
+
             const hranaRows = await stream.query(hranaStmt);
+            this._evictSqlCache();
             return resultSetFromHrana(hranaRows);
         } catch (e) {
             throw mapHranaError(e);
@@ -77,6 +90,7 @@ export class HranaClient implements Client {
     }
 
     async batch(stmts: Array<InStatement>): Promise<Array<ResultSet>> {
+        const useSqlCache = await this._useSqlCache();
         const stream = this.#openStream();
         try {
             const batch = stream.batch();
@@ -87,6 +101,10 @@ export class HranaClient implements Client {
             let lastStep = beginStep;
             const stmtPromises = stmts.map((stmt) => {
                 const hranaStmt = stmtToHrana(stmt);
+                if (useSqlCache) {
+                    this._applySqlCache(hranaStmt);
+                }
+
                 const stmtStep = batch.step()
                     .condition(hrana.BatchCond.ok(lastStep));
                 const stmtPromise = stmtStep.query(hranaStmt);
@@ -104,6 +122,7 @@ export class HranaClient implements Client {
             rollbackStep.run("ROLLBACK").catch(_ => undefined);
 
             await batch.execute();
+            this._evictSqlCache();
 
             const resultSets = [];
             for (const stmtPromise of stmtPromises) {
@@ -127,10 +146,11 @@ export class HranaClient implements Client {
     }
 
     async transaction(): Promise<HranaTransaction> {
+        const useSqlCache = await this._useSqlCache();
         const stream = this.#openStream();
         try {
             await stream.run("BEGIN");
-            return new HranaTransaction(stream);
+            return new HranaTransaction(this, stream, useSqlCache);
         } catch (e) {
             stream.close();
             throw mapHranaError(e);
@@ -143,6 +163,7 @@ export class HranaClient implements Client {
         }
 
         if (this.#client.closed) {
+            this.#sqlCache.clear();
             try {
                 this.#client = hrana.open(this.#url, this.#authToken);
             } catch (e) {
@@ -157,14 +178,49 @@ export class HranaClient implements Client {
         this.#client.close();
         this.closed = true;
     }
+
+    /** @private */
+    async _useSqlCache(): Promise<boolean> {
+        return await this.#client.getVersion() >= 2;
+    }
+
+    /** @private */
+    _applySqlCache(hranaStmt: hrana.Stmt): void {
+        if (typeof hranaStmt.sql !== "string") {
+            return;
+        }
+        const sqlText: string = hranaStmt.sql;
+
+        let sqlObj = this.#sqlCache.get(sqlText);
+        if (sqlObj === undefined) {
+            sqlObj = this.#client.storeSql(sqlText);
+            this.#sqlCache.set(sqlText, sqlObj);
+        }
+
+        if (sqlObj !== undefined) {
+            hranaStmt.sql = sqlObj;
+        }
+    }
+
+    /** @private */
+    _evictSqlCache(): void {
+        while (this.#sqlCache.size > sqlCacheCapacity) {
+            const sqlObj = this.#sqlCache.deleteLru()!;
+            sqlObj.close();
+        }
+    }
 }
 
 export class HranaTransaction implements Transaction {
+    #client: HranaClient;
     stream: hrana.Stream;
+    #useSqlCache: boolean;
 
     /** @private */
-    constructor(stream: hrana.Stream) {
+    constructor(client: HranaClient, stream: hrana.Stream, useSqlCache: boolean) {
+        this.#client = client;
         this.stream = stream;
+        this.#useSqlCache = useSqlCache;
     }
 
     async execute(stmt: InStatement): Promise<ResultSet> {
@@ -174,10 +230,19 @@ export class HranaTransaction implements Transaction {
                 "TRANSACTION_CLOSED",
             );
         }
-        const hranaStmt = stmtToHrana(stmt);
-        const hranaRows = await this.stream.query(hranaStmt)
-            .catch(e => { throw mapHranaError(e); });
-        return resultSetFromHrana(hranaRows);
+
+        try {
+            const hranaStmt = stmtToHrana(stmt);
+            if (this.#useSqlCache) {
+                this.#client._applySqlCache(hranaStmt);
+            }
+
+            const hranaRows = await this.stream.query(hranaStmt);
+            this.#client._evictSqlCache();
+            return resultSetFromHrana(hranaRows);
+        } catch (e) {
+            throw mapHranaError(e);
+        }
     }
 
     async rollback(): Promise<void> {
