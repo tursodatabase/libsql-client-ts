8a568be4bfbc47db14b4b1b894939ff4a8103002
Giovanni <giovanni.benussi@usach.cl>
2024-05-24 14:31:48 -0400

    WIP: Add MSW and merge main

---
+++ packages/libsql-client/src/__tests__/mocks.ts
@@ -0,0 +1,7 @@
+import { setupServer } from "msw/node";
+import { handlers } from "./handlers";
+
+export const server = setupServer(...handlers);
+server.events.on("request:start", ({ request }) => {
+  console.log("Outgoing:", request.method, request.url);
+});
