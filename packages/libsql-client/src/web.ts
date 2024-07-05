c42fad7992823b744dd0aad9f039282276e4f62b
Jamie Barton <jamie@notrab.dev>
2024-07-05 09:26:44 +0100

    eager commit

--- packages/libsql-client/src/web.ts
+++ packages/libsql-client/src/web.ts
@@ -10,25 +10,20 @@ import { _createClient as _createHttpClient } from "./http.js";
 export * from "@libsql/core/api";
 
 export function createClient(config: Config): Client {
-  return _createClient(expandConfig(config, true));
+    return _createClient(expandConfig(config, true));
 }
 
 /** @private */
 export function _createClient(config: ExpandedConfig): Client {
-  console.log("HELLO WORLD FROM WEB");
-  console.log("HELLO WORLD FROM WEB");
-  console.log("HELLO WORLD FROM WEB");
-  console.log("HELLO WORLD FROM WEB");
-
-  if (config.scheme === "ws" || config.scheme === "wss") {
-    return _createWsClient(config);
-  } else if (config.scheme === "http" || config.scheme === "https") {
-    return _createHttpClient(config);
-  } else {
-    throw new LibsqlError(
-      'The client that uses Web standard APIs supports only "libsql:", "wss:", "ws:", "https:" and "http:" URLs, ' +
-        `got ${JSON.stringify(config.scheme + ":")}. For more information, please read ${supportedUrlLink}`,
-      "URL_SCHEME_NOT_SUPPORTED",
-    );
-  }
+    if (config.scheme === "ws" || config.scheme === "wss") {
+        return _createWsClient(config);
+    } else if (config.scheme === "http" || config.scheme === "https") {
+        return _createHttpClient(config);
+    } else {
+        throw new LibsqlError(
+            'The client that uses Web standard APIs supports only "libsql:", "wss:", "ws:", "https:" and "http:" URLs, ' +
+                `got ${JSON.stringify(config.scheme + ":")}. For more information, please read ${supportedUrlLink}`,
+            "URL_SCHEME_NOT_SUPPORTED",
+        );
+    }
 }
