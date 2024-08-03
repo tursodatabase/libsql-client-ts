tursodatabase/libsql-client-ts

commit f4e4b277be9cb3538c85aab342bba7d26d7d9a47

add integration test & fix bug

packages/libsql-client/src/sqlite3.ts
@@ -62,7 +62,8 @@ export function _createClient(config: ExpandedConfig): Client {
         }
     }
 
-    const path = config.path;
+    // note: we must always prepend file scheme in order for SQLite3 to recognize :memory: connection query parameters
+    const path = `${config.scheme}:${config.path}`;
     const options = {
         authToken: config.authToken,
         encryptionKey: config.encryptionKey,

packages/libsql-client/src/__tests__/client.test.ts
@@ -294,13 +294,44 @@ describe("execute()", () => {
 
     // see issue https://github.com/tursodatabase/libsql/issues/1411
     test(
-        "execute transaction with in memory database",
-        withInMemoryClient(async (c) => {
-            await c.execute("CREATE TABLE t (a)");
-            const transaction = await c.transaction();
-            transaction.close();
-            await c.execute("SELECT * FROM t");
-        }),
+        "execute transaction against in memory database with shared cache",
+        withClient(
+            async (c) => {
+                await c.execute("CREATE TABLE t (a)");
+                const transaction = await c.transaction();
+                transaction.close();
+                await c.execute("SELECT * FROM t");
+            },
+            { url: "file::memory:?cache=shared" },
+        ),
+    );
+    test(
+        "execute transaction against in memory database with private cache",
+        withClient(
+            async (c) => {
+                await c.execute("CREATE TABLE t (a)");
+                const transaction = await c.transaction();
+                transaction.close();
+                expect(() => c.execute("SELECT * FROM t")).rejects.toThrow(
+                    LibsqlError,
+                );
+            },
+            { url: "file::memory:?cache=private" },
+        ),
+    );
+    test(
+        "execute transaction against in memory database with default cache",
+        withClient(
+            async (c) => {
+                await c.execute("CREATE TABLE t (a)");
+                const transaction = await c.transaction();
+                transaction.close();
+                expect(() => c.execute("SELECT * FROM t")).rejects.toThrow(
+                    LibsqlError,
+                );
+            },
+            { url: ":memory:" },
+        ),
     );
 });
 

commit c103045967a2b4f0d49ba89c2022647ce07628f9

Fix WAL index path for embedded replicas
People are reporting "No such file or directory" error on sync because
commit f4e4b27 ("add integration test & fix bug") accidentally changed
the path where we put the WAL index in.

Fixes #244

Co-authored-by: Nikita Sivukhin <sivukhin@turso.tech>

packages/libsql-client/src/sqlite3.ts
@@ -12,11 +12,11 @@ import type {
     Value,
     InValue,
     InStatement,
-    InArgs
+    InArgs,
 } from "@libsql/core/api";
 import { LibsqlError } from "@libsql/core/api";
 import type { ExpandedConfig } from "@libsql/core/config";
-import { expandConfig } from "@libsql/core/config";
+import { expandConfig, isInMemoryConfig } from "@libsql/core/config";
 import {
     supportedUrlLink,
     transactionModeToBegin,
@@ -63,8 +63,20 @@ export function _createClient(config: ExpandedConfig): Client {
         }
     }
 
-    // note: we must always prepend file scheme in order for SQLite3 to recognize :memory: connection query parameters
-    const path = `${config.scheme}:${config.path}`;
+    let isInMemory = isInMemoryConfig(config);
+    if (isInMemory && config.syncUrl) {
+        throw new LibsqlError(
+            `Embedded replica must use file for local db but URI with in-memory mode were provided instead: ${config.path}`,
+            "URL_INVALID",
+        );
+    }
+
+    let path = config.path;
+    if (isInMemory) {
+        // note: we should prepend file scheme in order for SQLite3 to recognize :memory: connection query parameters
+        path = `${config.scheme}:${config.path}`;
+    }
+
     const options = {
         authToken: config.authToken,
         encryptionKey: config.encryptionKey,
@@ -109,17 +121,20 @@ export class Sqlite3Client implements Client {
     async execute(stmt: InStatement): Promise<ResultSet>;
     async execute(sql: string, args?: InArgs): Promise<ResultSet>;
 
-    async execute(stmtOrSql: InStatement | string, args?: InArgs): Promise<ResultSet> {
-      let stmt: InStatement;
+    async execute(
+        stmtOrSql: InStatement | string,
+        args?: InArgs,
+    ): Promise<ResultSet> {
+        let stmt: InStatement;
 
-      if (typeof stmtOrSql === 'string') {
-        stmt = {
-            sql: stmtOrSql,
-            args: args || []
-        };
-      } else {
-          stmt = stmtOrSql;
-      }
+        if (typeof stmtOrSql === "string") {
+            stmt = {
+                sql: stmtOrSql,
+                args: args || [],
+            };
+        } else {
+            stmt = stmtOrSql;
+        }
 
         this.#checkNotClosed();
         return executeStmt(this.#getDb(), stmt, this.#intMode);
@@ -208,19 +223,22 @@ export class Sqlite3Transaction implements Transaction {
     }
 
     async execute(stmt: InStatement): Promise<ResultSet>;
-        async execute(sql: string, args?: InArgs): Promise<ResultSet>;
+    async execute(sql: string, args?: InArgs): Promise<ResultSet>;
 
-        async execute(stmtOrSql: InStatement | string, args?: InArgs): Promise<ResultSet> {
-          let stmt: InStatement;
+    async execute(
+        stmtOrSql: InStatement | string,
+        args?: InArgs,
+    ): Promise<ResultSet> {
+        let stmt: InStatement;
 
-          if (typeof stmtOrSql === 'string') {
+        if (typeof stmtOrSql === "string") {
             stmt = {
                 sql: stmtOrSql,
-                args: args || []
+                args: args || [],
             };
-          } else {
-              stmt = stmtOrSql;
-          }
+        } else {
+            stmt = stmtOrSql;
+        }
 
         this.#checkNotClosed();
         return executeStmt(this.#database, stmt, this.#intMode);

packages/libsql-core/src/config.ts
@@ -28,6 +28,13 @@ type queryParamsDef = { [key: string]: queryParamDef };
 
 const inMemoryMode = ":memory:";
 
+export function isInMemoryConfig(config: ExpandedConfig): boolean {
+    return (
+        config.scheme === "file" &&
+        (config.path === ":memory:" || config.path.startsWith(":memory:?"))
+    );
+}
+
 export function expandConfig(
     config: Readonly<Config>,
     preferHttp: boolean,