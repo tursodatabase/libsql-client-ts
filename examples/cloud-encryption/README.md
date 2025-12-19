ec8a6659121b9a50d1329a3a4b8fb08efdc56586
Avinash Sajjanshetty <avi@turso.tech>
2025-12-19 16:38:24 +0530

    Add examples of cloud encryption

---
+++ examples/cloud-encryption/README.md
@@ -0,0 +1,25 @@
+# Cloud Encryption
+
+These examples demonstrates how to use Turso Cloud encryption.
+
+Visit the documentation here - [Cloud Encryption](https://docs.turso.tech/cloud/encryption)
+
+## Install Dependencies
+
+```bash
+npm i
+```
+
+## Running
+
+Execute the example which operates over remotely encrypted database:
+
+```bash
+node remote.mjs
+```
+
+Cloud encryption also supports sync:
+
+```bash
+node sync.mjs
+```
---
+++ examples/cloud-encryption/sync.mjs
@@ -0,0 +1,22 @@
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+    url: "file:local.db",
+    syncUrl: process.env.TURSO_DATABASE_URL,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+    remoteEncryptionKey: process.env.TURSO_REMOTE_ENCRYPTION_KEY,
+});
+
+await client.batch(
+    [
+        "CREATE TABLE IF NOT EXISTS users (email TEXT)",
+        "INSERT INTO users VALUES ('first@example.com')",
+        "INSERT INTO users VALUES ('second@example.com')",
+        "INSERT INTO users VALUES ('third@example.com')",
+    ],
+    "write",
+);
+
+const result = await client.execute("SELECT * FROM users");
+
+console.log("Users:", result.rows);
